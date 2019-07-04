d536e46e28b2700075ab06ff2160a97769f592b0
Michal Muller <michal@towoo.dk>
2019-07-04 21:35:56 +0200

    displaying uploaded images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-if="loading">Loading ...</div>
+    <div else class="images">
+      <img v-for="(img, index) in images" :key="index" :src="img.url" :alt="img.name" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import { mapState } from "vuex";
+import firebase from "firebase";
+import db from "@/firebase/firebaseInit";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  },
+
   data() {
-    return {};
+    return {
+      images: [],
+      loading: true
+    };
   },
   methods: {},
   computed: {
     ...mapState({
       user: state => state.user.user
     })
+  },
+  mounted() {
+    db.collection("icons")
+      .get()
+      .then(snap => {
+        snap.forEach(doc => {
+          var id = { id: doc.id };
+          var image = { ...id, ...doc.data() };
+          this.images.push(image);
+        });
+      })
+      .then(() => {
+        this.loading = false;
+      });
   }
 };
 </script>
+<style lang="scss" scoped>
+$grey: #ddd;
+
+.images {
+  display: flex;
+}
+img {
+  padding: 10px;
+  margin: 5px;
+  border: 1px solid $grey;
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+}
+</style>
