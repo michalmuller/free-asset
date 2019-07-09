508cb88bcf97f983681516ad3f3f0a323dd43b13
Michal Muller <michal@towoo.dk>
2019-07-09 21:00:44 +0200

    minor styling changes again

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="home container">
     <div v-if="loading">Loading ...</div>
-    <div v-else class="flex flex-wrap mt-4">
+    <div v-else class="flex flex-wrap mt-8">
       <div v-for="(img, index) in images" :key="index" class="w-1/2 sm:w-1/4 md:w-1/4 lg:w-1/6">
         <div class="m-2 img-wrapper rounded shadow-md bg-white">
           <div class="img-container">
             <div class="img-square">
               <img class="p-4" :src="img.url" :alt="img.displayName" />
             </div>
-          </div>
-          <div class="img-actions pt-2 pl-1 pb-1 flex justify-between items-end">
-            <p class="text-xs font-semibold">{{img.displayName}}</p>
-            <img
-              @click="download(img)"
-              class="pr-1 pl-2 cursor-pointer"
-              src="../../public/img/icons/download.svg"
-            />
+            <div class="cursor-pointer btn-download">
+              <img @click="download(img)" src="../../public/img/icons/download.svg" />
+            </div>
           </div>
         </div>
       </div>
@@ -82,6 +77,9 @@ export default {
 <style lang="scss" scoped>
 .img-wrapper {
   padding: 0.35rem !important;
+  &:hover .btn-download {
+    display: flex;
+  }
   .img-container {
     position: relative;
     width: 100%;
@@ -104,6 +102,25 @@ export default {
         width: 100%;
         border: 1px solid #e9e9e9;
       }
+    }
+  }
+  .btn-download {
+    display: none;
+    background-color: #f93466;
+    height: 32px;
+    width: 32px;
+    position: absolute;
+    right: 0.5rem;
+    bottom: 0.5rem;
+    border-radius: 4px;
+    align-items: center;
+    &:hover {
+      background-color: #e42555;
+    }
+    img {
+      width: 18px;
+      margin-left: 7px;
+      margin-right: 7px;
     }
   }
 }
